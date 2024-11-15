ff2dbabacde75379f4667148df0b6166fa0f3148
António Lira Fernandes <alf@esmonserrate.org>
2024-11-15 09:59:13 +0000

    first commit

---
+++ .history/routes/routes_20241108092134.php
@@ -0,0 +1,22 @@
+<?php
+use src\Route as Route;
+use classes\authentication\Authentication;
+
+//Zona sem autenticação
+Route::get('/', function(){require _CAMINHO_TEMPLATE. "index.html";});
+Route::get('/batatas', function(){require _CAMINHO_TEMPLATE. "index.html";});
+Route::get('/Erros/401', function(){require _CAMINHO_ERROS. "erro401.php";});
+Route::get('/Erros/404', function(){require _CAMINHO_ERROS. "erro404.php";});
+
+Route::get(['set' => '/base/index', 'as' => 'base.index'], 'Controller@index'); 
+Route::get(['set' => '/base/show/{id}', 'as' => 'base.show'], 'Controller@show'); 
+
+//Artigos
+Route::get(['set' => '/artigos/numeros', 'as' => 'artigos.contarArtigos'], 'ControllerArtigos@contarArtigos');
+Route::get(['set' => '/artigo/{id}/ver', 'as' => 'artigos.ArtigoVer'], 'ControllerArtigos@ArtigoVer');                      //web service
+Route::get('/artigo/ver/{id}', function(){  require _CAMINHO_TEMPLATE1. "artigo.php";});          //ver artigo
+Route::get('/artigo/ver/', function(){  require _CAMINHO_TEMPLATE1. "artigo.php";});          //ver artigo
+
+//Users
+Route::get(['set' => '/users/contar', 'as' => 'users.contarUsers'], 'ControllerUser@contarUsers'); 
+Route::get(['set' => '/users/lista', 'as' => 'users.listOfUsers'], 'ControllerUser@listOfUsers');
